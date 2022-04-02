364a814e3b3833f460d6da34201daa40357ab6d0
tksh0113 <101071261+tksh0113@users.noreply.github.com>
2022-04-02 17:42:35 +0900

    Add ec2 for ci test

--- aws_ec2.tf
+++ aws_ec2.tf
@@ -2,3 +2,8 @@ resource "aws_instance" "example" {
   ami           = "ami-0ab0bbbd329f565e6"
   instance_type = "t2.micro"
 }
+
+resource "aws_instance" "example2-for-ci-test" {
+  ami           = "ami-0ab0bbbd329f565e6"
+  instance_type = "t2.micro"
+}
