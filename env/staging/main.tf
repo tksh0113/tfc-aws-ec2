f4949122c89932e970b54ead710aa1333df240bc
tksh0113 <101071261+tksh0113@users.noreply.github.com>
2022-05-23 10:59:59 +0900

    staging に backendを TFCに

--- env/staging/main.tf
+++ env/staging/main.tf
@@ -4,3 +4,13 @@ module "vpc" {
   cidr = "172.16.0.0/24"
   env  = "staging"
 }
+
+terraform {
+  backend "remote" {
+    organization = "asutake"
+
+    workspaces {
+      name = "tfc-aws-ec2-staging"
+    }
+  }
+}
